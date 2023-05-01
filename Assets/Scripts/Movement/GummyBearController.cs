57482fde408104e1931b4dd5684c860a17bc75f3
[Yifan] <1347933756@qq.com>
2023-05-01 11:48:18 +0200

    try to make body stable

--- Assets/Scripts/Movement/GummyBearController.cs
+++ Assets/Scripts/Movement/GummyBearController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,10 +8,16 @@ public class GummyBearController : MonoBehaviour
     public float moveForce = 50.0f;
     public float stretchForce = 50.0f;
 
+    public float maxStretchDistancePercent = 1.5f;
+    private bool isInitialized = false;
+
+
     private Vector2 moveDirection;
     private Vector2 stretchDirection;
 
-    private JellySprite m_JellySprite;
+    private UnityJellySprite m_JellySprite;
+    private List<Vector2> initialReferencePointOffsets;
+
 
     private GummyBearActions gummyBearActions;
 
@@ -27,10 +35,67 @@ public class GummyBearController : MonoBehaviour
         gummyBearActions.GummyBearTest.StretchVertical.canceled += ctx => stretchDirection.y = 0;
     }
 
-    void Start()
+    //void Start()
+    //{
+    //    if (GetComponent<UnityJellySprite>() != null)
+    //    {
+    //        Debug.Log("we can find UnityJellySprite");
+    //    }
+
+    //    m_JellySprite = GetComponent<UnityJellySprite>();
+
+    //    initialReferencePointOffsets = new List<Vector2>();
+
+    //    if (m_JellySprite.ReferencePoints == null)
+    //    {
+    //        Debug.Log("there' no reference point");
+    //    }
+    //    else
+    //    {
+    //        foreach (var refPoint in m_JellySprite.ReferencePoints)
+    //        {
+    //            if (refPoint != m_JellySprite.CentralPoint)
+    //            {
+    //                Vector2 initialOffset = refPoint.transform.position - m_JellySprite.CentralPoint.transform.position;
+    //                initialReferencePointOffsets.Add(initialOffset);
+    //            }
+    //        }
+    //    }
+    //}
+
+    void Update()
     {
-        m_JellySprite = GetComponent<JellySprite>();
+        if (!isInitialized)
+        {
+            if (GetComponent<UnityJellySprite>() != null)
+            {
+                Debug.Log("we can find UnityJellySprite");
+            }
+
+            m_JellySprite = GetComponent<UnityJellySprite>();
+
+            initialReferencePointOffsets = new List<Vector2>();
+
+            if (m_JellySprite.ReferencePoints == null)
+            {
+                Debug.Log("there' no reference point");
+            }
+            else
+            {
+                foreach (var refPoint in m_JellySprite.ReferencePoints)
+                {
+                    if (refPoint != m_JellySprite.CentralPoint)
+                    {
+                        Vector2 initialOffset = refPoint.transform.position - m_JellySprite.CentralPoint.transform.position;
+                        initialReferencePointOffsets.Add(initialOffset);
+                    }
+                }
+
+                isInitialized = true;
+            }
+        }
     }
+
 
     void OnEnable()
     {
@@ -64,6 +129,7 @@ public class GummyBearController : MonoBehaviour
         float totalStretchForce = stretchDirection.x + stretchDirection.y;
         if (Mathf.Abs(totalStretchForce) > Mathf.Epsilon)
         {
+            int refPointIndex = 0;
             foreach (var refPoint in m_JellySprite.ReferencePoints)
             {
                 if (refPoint.Body2D != null && refPoint != m_JellySprite.CentralPoint)
@@ -77,10 +143,23 @@ public class GummyBearController : MonoBehaviour
                     float stretchForceX = horizontalStretchForce - verticalStretchForce;
                     float stretchForceY = verticalStretchForce + horizontalStretchForce;
 
-                    refPoint.Body2D.AddForce(new Vector2(stretchForceX, stretchForceY));
+                    // Calculate the current distance from the central point
+                    float currentDistance = Vector2.Distance(refPoint.Body2D.position, m_JellySprite.CentralPoint.Body2D.position);
+
+                    // Calculate the initial distance from the central point using the stored initial offsets
+                    float initialDistance = initialReferencePointOffsets[refPointIndex].magnitude;
+
+                    // Calculate the maximum allowed stretch distance
+                    float maxStretchDistance = initialDistance * maxStretchDistancePercent;
+
+                    // Apply the stretch force if the current distance is less than the maximum allowed stretch distance
+                    if (currentDistance < maxStretchDistance)
+                    {
+                        refPoint.Body2D.AddForce(new Vector2(stretchForceX, stretchForceY));
+                    }
                 }
+                refPointIndex++;
             }
         }
     }
-
 }
